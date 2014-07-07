c5be731600df4680e9c6fe20e3e168d99ab04cb9
John Morrison <jrgmorrison@gmail.com>
2014-07-06 20:14:50 -0700

    add test for 2,3,4-byte UTF-8 content

--- test/db.js
+++ test/db.js
@@ -3,8 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const db = require('../lib/db');
+const P = require('../lib/promise');
+const assert = require('insist');
+const crypto = require('crypto');
 
 /*global describe,it*/
+
+function randomString(len) {
+  return crypto.randomBytes(Math.ceil(len)).toString('hex');
+}
 
 describe('db', function() {
 
@@ -15,4 +22,44 @@ describe('db', function() {
       });
     });
   });
+
+  describe('utf-8', function() {
+
+    function makeTest(clientId, clientName) {
+      return function() {
+        var data = {
+          id: clientId,
+          name: clientName,
+          secret: randomString(32),
+          imageUri: 'https://example.domain/logo',
+          redirectUri: 'https://example.domain/return?foo=bar',
+          whitelisted: true
+        };
+
+        return db.registerClient(data)
+          .then(function(c) {
+            assert.equal(c.id.toString('hex'), clientId);
+            assert.equal(c.name, clientName);
+            return db.getClient(c.id);
+          })
+          .then(function(cli) {
+            assert.equal(cli.id.toString('hex'), clientId);
+            assert.equal(cli.name, clientName);
+            return db.removeClient(clientId);
+          })
+          .then(function() {
+            return db.getClient(clientId)
+              .then(function(cli) {
+                assert.equal(void 0, cli);
+              });
+          });
+      }
+    }
+
+    it('2-byte encoding preserved', makeTest(randomString(8), 'Düsseldorf'));
+    it('3-byte encoding preserved', makeTest(randomString(8), '北京')); // Beijing
+    it('4-byte encoding preserved', makeTest(randomString(8), '𝛑')); // U+1D6D1 MATHEMATICAL BOLD SMALL PI
+
+  });
+
 });
