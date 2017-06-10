4eb36cfc598d4a4f9a4ef14a9a54dd72d29b268e
vladikoff <vlad.filippov@gmail.com>
2017-06-09 22:33:58 -0400

    feat(pkce): add mysql statements

---
+++ lib/db/mysql/patches/patch-018-017.sql
@@ -0,0 +1,9 @@
+--  Drop `publicClient` column from the `clients` table.
+
+-- ALTER TABLE publicClient DROP COLUMN publicClient,
+-- ALGORITHM = INPLACE, LOCK = NONE;
+
+-- ALTER TABLE publicClient DROP COLUMN publicClient,
+-- ALGORITHM = INPLACE, LOCK = NONE;
+
+-- UPDATE dbMetadata SET value = '17' WHERE name = 'schema-patch-level';
