a1c496a44a76ab5ac23d7d6d596e9f96956fbfb7
vladikoff <vlad.filippov@gmail.com>
2017-06-18 19:22:18 -0400

    feat(pkce): updates based on review

--- lib/db/mysql/patches/patch-017-018.sql
+++ lib/db/mysql/patches/patch-017-018.sql
@@ -2,10 +2,10 @@
 ALTER TABLE clients ADD COLUMN publicClient BOOLEAN DEFAULT FALSE NOT NULL AFTER canGrant;
 UPDATE clients SET publicClient=false;
 
---  Add `code_challenge_method` and `code_challenge` column to the `codes` table.
+--  Add `codeChallengeMethod` and `codeChallenge` column to the `codes` table.
 ALTER TABLE codes
-ADD COLUMN code_challenge_method VARCHAR(256) AFTER offline,
-ADD COLUMN code_challenge VARCHAR(256) AFTER code_challenge_method,
+ADD COLUMN codeChallengeMethod VARCHAR(256) AFTER offline,
+ADD COLUMN codeChallenge VARCHAR(256) AFTER codeChallengeMethod,
 ALGORITHM = INPLACE, LOCK = NONE;
 
 UPDATE dbMetadata SET value = '18' WHERE name = 'schema-patch-level';
